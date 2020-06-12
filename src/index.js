7483d5594d566a2d92c7d7928c55b8f42a7408d3
iamamit-107 <ahmed.amit107@gmail.com>
2020-06-12 16:32:33 +0600

    reducer and actions are created

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import store from "./store";
+import * as actions from "./actionTypes";
+
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed", store.getState());
+});
+
+store.dispatch({
+    type: actions.BUG_ADDED,
+    payload: {
+        description: "bug1",
+    },
+});
+
+unsubscribe();
+
+store.dispatch({
+    type: actions.BUG_REMOVED,
+    payload: {
+        id: 1,
+    },
+});
+console.log(store.getState());
