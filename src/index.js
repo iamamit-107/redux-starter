353f17d3500d6b77eef0cc2c7265a9661ec78b87
iamamit-107 <ahmed.amit107@gmail.com>
2020-06-12 16:41:50 +0600

    action reformatted

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved } from "./actions";
 
 const unsubscribe = store.subscribe(() => {
     console.log("Store changed", store.getState());
 });
 
-store.dispatch({
-    type: actions.BUG_ADDED,
-    payload: {
-        description: "bug1",
-    },
-});
+store.dispatch(bugAdded("bug 2"));
 
 unsubscribe();
 
-store.dispatch({
-    type: actions.BUG_REMOVED,
-    payload: {
-        id: 1,
-    },
-});
+store.dispatch(bugRemoved(1));
 console.log(store.getState());
